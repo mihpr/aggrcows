b1a6ec9e18d7b6f3190038ec7e8630c64b4f0e80
Mihails Prihodko <mihails.prihodko@gmail.com>
2021-02-20 18:52:36 +0200

    File read was improved, support of multiple test cases was added

--- main.c
+++ main.c
@@ -3,75 +3,98 @@
 #include <stdint.h>
 #include <string.h>
 
+// Description of the task
+// https://www.spoj.com/problems/AGGRCOW/
 
-// Global defines
-#define INPUT_FILE "./input.txt"
+#define INPUT_FILE "D:\\data\\git\\aggrcows\\input.txt"
+//#define INPUT_FILE "./input.txt" // TODO: delete
 #define MAX_NUM_STALLS (100000)
 
 #define COW_PLACED_BIT_MASK  (1 << 31)
 #define COW_POSITION_MASK    (~COW_PLACED_BIT_MASK)
 
+#define STR_BUF_SIZE (256)
 
-// Global settings
-int num_testcases;
-int num_stalls;                  // N [2; 100 000]
-int num_cows;                    // C [2; N]
-uint32_t aStall[MAX_NUM_STALLS]; // Stall positions [x1; xN], where xi [0; 1 000 000 000]
-
-
-void init_buffers()
+void init_buffers(uint32_t *p_stall, size_t buf_len)
 {
-    memset(&aStall[0], 0, sizeof(aStall));
-
+    memset(&p_stall[0], 0, sizeof(uint32_t) * buf_len);
 }
 
-#define STR_BUF_SIZE (255)
-// This function reads data from input file and creates test cases.
-// Currently it can handle only one test case.
-// TODO: Implement of handling more than one test case.
-// TODO: Proper error handling.
-void read_input_data()
+
+void dbg_print_stalls(uint32_t *p_stall, size_t stall_cnt)
 {
-    FILE *fp;
+    printf("stalls:\n");
+    int i;
+    for (i = 0; i < (int)stall_cnt; i++)
+    {
+        printf("    %i: [%i]\n", i, p_stall[i]);
+    }
+    printf("\n");
+}
+
+int read_num_testcases(FILE * fp)
+{
     char buff[STR_BUF_SIZE];
 
-    fp = fopen(INPUT_FILE, "r");
-
-    // 1-st line: number of test cases
-    if (fgets(buff, STR_BUF_SIZE, fp) != NULL)
+    // Read number of test cases
+    if (fgets(buff, STR_BUF_SIZE, fp) == NULL)
     {
-       num_testcases = atoi(buff);
+        printf("Error: failed to read the 1-st line of the input file: number of test cases.\n");
+        fclose(fp);
+        exit(EXIT_FAILURE);
     }
 
-    if (fgets(buff, STR_BUF_SIZE, fp) != NULL)
+    return atoi(buff);
+}
+
+void read_test_case(FILE * fp, int *p_num_stalls, int *p_num_cows, uint32_t *p_stall)
+{
+    char buff[STR_BUF_SIZE];
+    char *pToken;
+    char delimiters[] = " ,.-";
+
+    // Two space-separated integers: number of stalls and number of cows.
+    if (fgets(buff, STR_BUF_SIZE, fp) == NULL)
+
     {
-        char *pToken;
-        char delimiters[] = " ,.-";
-        // printf ("Splitting string \"%s\" into tokens:\n",str);
-        pToken = strtok(buff, delimiters);
-        if (pToken != NULL) // num stalls, N
-        {
-            num_stalls = atoi(pToken);
-            pToken = strtok(NULL, delimiters);
-        }
-        if (pToken != NULL) // num cows, C
-        {
-            num_cows = atoi(pToken);
-        }
+        printf("Error: failed to read the number of stalls and number of cowss.\n");
+        fclose(fp);
+        exit(EXIT_FAILURE);
     }
+
+    // printf ("Splitting string \"%s\" into tokens:\n",str);
+    pToken = strtok(buff, delimiters);
+    if (pToken == NULL) // num stalls, N
+    {
+        printf("Error: failed to get the number of stalls.\n");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+    *p_num_stalls = atoi(pToken);
+
+    pToken = strtok(NULL, delimiters);
+
+    if (pToken == NULL) // num cows, C
+    {
+        printf("Error: failed to get the number of cows.\n");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+    *p_num_cows = atoi(pToken);
 
     // Read stall positions
     int i, stall_position;
-    for (i = 0; i < num_stalls; i++)
+    for (i = 0; i < *p_num_stalls; i++)
     {
-        if (fgets(buff, STR_BUF_SIZE, fp) != NULL)
+        if (fgets(buff, STR_BUF_SIZE, fp) == NULL)
         {
-            stall_position = atoi(buff);
-            aStall[i] = stall_position;
+            printf("Error: failed to read the number of stalls at position [%i].\n", i);
+            fclose(fp);
+            exit(EXIT_FAILURE);
         }
+        stall_position = atoi(buff);
+        p_stall[i] = stall_position;
     }
-
-    fclose(fp);
 }
 
 
@@ -94,18 +117,18 @@ void bubble_sort_uint32(uint32_t *arr, int n)
    }
 }
 
-uint32_t solve()
+uint32_t solve(int num_stalls, int num_cows, uint32_t *p_stall)
 {
     uint32_t best_known_place_distance;
     int i, j;
     int cows_left = num_cows; // a counter for cows not placed yet into the stalls
 
     // Stalls are unsorted, they should be sorted for convenience
-    bubble_sort_uint32(aStall, num_stalls);
+    bubble_sort_uint32(&p_stall[0], num_stalls);
 
     // There are at least 2 cows and they are placed at the first and the last stalls
-    aStall[0] |= COW_PLACED_BIT_MASK;
-    aStall[num_stalls-1] |= COW_PLACED_BIT_MASK;
+    p_stall[0] |= COW_PLACED_BIT_MASK;
+    p_stall[num_stalls-1] |= COW_PLACED_BIT_MASK;
     cows_left -= 2;
 
     // While there are still cows to be placed,
@@ -120,7 +143,7 @@ uint32_t solve()
         for (i = 0; i < num_stalls; i++)
         {
             // If a cow is placed here, it is impossible to place another one here, skip this stall
-            if (aStall[i] & COW_PLACED_BIT_MASK)
+            if (p_stall[i] & COW_PLACED_BIT_MASK)
             {
                 if (cows_left > 0)
                 {
@@ -135,9 +158,9 @@ uint32_t solve()
             {
                 for (j = i-1; j >= 0; j--) // search for a placed cow
                 {
-                    if (aStall[j] & COW_PLACED_BIT_MASK)
+                    if (p_stall[j] & COW_PLACED_BIT_MASK)
                     {
-                        left_distance = (aStall[i] & COW_POSITION_MASK) - (aStall[j] & COW_POSITION_MASK);
+                        left_distance = (p_stall[i] & COW_POSITION_MASK) - (p_stall[j] & COW_POSITION_MASK);
                         break;
                     }
                 }
@@ -149,9 +172,9 @@ uint32_t solve()
             {
                 for (j = i+1; j < num_stalls; j++) // search for a placed cow
                 {
-                    if (aStall[j] & COW_PLACED_BIT_MASK)
+                    if (p_stall[j] & COW_PLACED_BIT_MASK)
                     {
-                        right_distance = (aStall[j] & COW_POSITION_MASK) - (aStall[i] & COW_POSITION_MASK);
+                        right_distance = (p_stall[j] & COW_POSITION_MASK) - (p_stall[i] & COW_POSITION_MASK);
                         break;
                     }
                 }
@@ -182,7 +205,7 @@ uint32_t solve()
         // Place the cow
         if (cows_left > 0)
         {
-            aStall[best_known_place_idx] |= COW_PLACED_BIT_MASK;
+            p_stall[best_known_place_idx] |= COW_PLACED_BIT_MASK;
         }
 
 //        printf("Result, cows left: %i\n", cows_left);
@@ -200,13 +223,35 @@ uint32_t solve()
 
 int main()
 {
-    init_buffers();
-    read_input_data();
-    printf("Test cases: %i, cows: %i, stalls: %i\n", num_testcases, num_cows, num_stalls);
+    int num_stalls;                  // N [2; 100 000]
+    int num_cows;                    // C [2; N]
+    uint32_t a_stall[MAX_NUM_STALLS]; // Stall positions [x1; xN], where xi [0; 1 000 000 000]
 
-    uint32_t result = solve();
-    printf("Output: %u", result);
+    FILE * fp;
+    fp = fopen(INPUT_FILE, "r");
 
+    if (fp == NULL)
+    {
+       printf("Error: no input file was found at the given path [%s]\n", INPUT_FILE);
+       exit(EXIT_FAILURE);
+    }
 
+    int num_testcases = read_num_testcases(fp);
+    printf("Number of test cases: %i\n\n", num_testcases);
+    int t;
+    for (t = 1; t <= num_testcases; t++)
+    {
+        init_buffers(&a_stall[0], MAX_NUM_STALLS);
+
+        printf("Test case: [%i]\n", t);
+        read_test_case(fp, &num_stalls, &num_cows,  &a_stall[0]);
+        printf("num_stalls: [%i], num_cows: [%i]\n", num_stalls, num_cows);
+//        dbg_print_stalls(&a_stall[0], num_stalls);
+
+        uint32_t result = solve(num_stalls, num_cows, &a_stall[0]);
+        printf("Result: %u\n\n", result);
+    }
+
+    fclose(fp);
     return 0;
 }
