b5889681d404ba2c912ead9ba806f13a78714666
Mihails Prihodko <mihails.prihodko@gmail.com>
2021-03-02 20:58:16 +0200

    The solution was fixed according to the requirements from SPOJ

--- main.c
+++ main.c
@@ -7,46 +7,15 @@
 
 #define MAX_NUM_STALLS (100000)
 
+#define INPUT_FROM_FILE (0) // 0: no, 1: yes
+#if INPUT_FROM_FILE == (1)
+    #define INPUT_FILE "D:\\data\\git\\aggrcows\\input.txt"
+    //#define INPUT_FILE "D:\\data\\git\\aggrcows\\test\\testcase.txt"
+#endif
+
 int cmp_stalls (const void *a, const void *b)
 {
    return (*(int*)a - *(int*)b);
-}
-
-int place_cows(int num_stalls, int num_cows, uint32_t *p_stall, uint32_t min_distance)
-{
-    // Try placing all the cows in stalls starting from the stall with the minimal number and
-    // ensuring that the distance between any two neighbour cows is greater or equal to the given minimal distance.
-
-    // There are at least 2 cows.
-    // The first stall is taken by the first cow. Start from the second stall and the second cow (with index 1).
-    int stall_idx;
-    int last_taken_stall_idx = 0;
-    int cows_placed = 1;
-    for (stall_idx = 1; stall_idx < num_stalls && cows_placed < num_cows; stall_idx++)
-    {
-        // Calculate the distance between the current stall and the stall, where the previous cow was placed
-        uint32_t distance = p_stall[stall_idx] - p_stall[last_taken_stall_idx];
-        // Place current cow, in case the distance is at least the given minimal distance
-        if (distance >= min_distance)
-        {
-            cows_placed++;
-            last_taken_stall_idx = stall_idx;
-        }
-    }
-
-//    return cows_placed;
-    if (cows_placed == num_cows && (stall_idx + 1) == num_stalls)
-    {
-        return 0; // all cows placed, no stalls left
-    }
-    else if (cows_placed == num_cows)
-    {
-        return 1; // all cows placed, some stalls left
-    }
-    else
-    {
-        return -1; // not enough stalls to place the cows
-    }
 }
 
 uint32_t solve(int num_stalls, int num_cows, uint32_t *p_stall)
@@ -56,33 +25,91 @@ uint32_t solve(int num_stalls, int num_cows, uint32_t *p_stall)
 
     uint32_t min = 1;
     uint32_t max = p_stall[num_stalls - 1] - p_stall[0];
-    uint32_t mid = (max - min) / 2;
-
+    uint32_t mid;
+    uint32_t best_solution = 1;
 
     while (min <= max)
     {
-        mid = (max - min) / 2;
-        int place_result = place_cows(num_stalls, num_cows, p_stall, mid);
-        if (place_result == 1)
+        mid = min + (max - min) / 2;
         {
-            min = mid + 1; // all cows placed, some stalls left
-        }
-        else if (place_result == -1)
-        {
-            max = mid - 1; // // not enough stalls to place the cows
-        }
-        else
-        {
-            return mid;
+            // Try placing all the cows in stalls starting from the stall with the minimal number and
+            // ensuring that the distance between any two neighbour cows is greater or equal to the given minimal distance.
+
+            // There are at least 2 cows.
+            // The first stall is taken by the first cow. Start from the second stall and the second cow (with index 1).
+            int stall_idx;
+            int last_taken_stall_idx = 0;
+            int cows_placed = 1;
+
+            for (stall_idx = 1; stall_idx < num_stalls && cows_placed < num_cows; stall_idx++)
+            {
+                // Calculate the distance between the current stall and the stall, where the previous cow was placed
+                uint32_t distance = p_stall[stall_idx] - p_stall[last_taken_stall_idx];
+                // Place current cow, in case the distance is at least the given minimal distance
+                if (distance >= mid)
+                {
+                    cows_placed++;
+                    last_taken_stall_idx = stall_idx;
+                }
+            }
+
+            if (cows_placed == num_cows)
+            {
+                best_solution = mid;
+                min = mid + 1;
+            }
+            else // if (cows_placed < num_cows)
+            {
+                max = mid - 1;
+            }
         }
     }
-    return mid;
+    return best_solution;
 }
 
+#if INPUT_FROM_FILE == (1)
 int main()
 {
-    int num_stalls;                  // N [2; 100 000]
-    int num_cows;                    // C [2; N]
+    FILE * fp;
+    fp = fopen(INPUT_FILE, "r");
+
+    if (fp == NULL)
+    {
+       printf("Error: no input file was found at the given path [%s]\n", INPUT_FILE);
+       exit(EXIT_FAILURE);
+    }
+
+    int num_stalls;                   // N [2; 100 000]
+    int num_cows;                     // C [2; N]
+    uint32_t a_stall[MAX_NUM_STALLS]; // Stall positions [x1; xN], where xi [0; 1 000 000 000]
+
+    int num_testcases;
+    fscanf(fp, "%d", &num_testcases);
+    int t;
+    for (t = 0; t < num_testcases; t++)
+    {
+        fscanf(fp, "%d %d", &num_stalls, &num_cows);
+
+        // Read stall positions
+        int i, stall_position;
+        for (i = 0; i < num_stalls; i++)
+        {
+            fscanf(fp, "%d", &stall_position);
+            a_stall[i] = stall_position;
+        }
+        uint32_t result = solve(num_stalls, num_cows, &a_stall[0]);
+        printf("%u\n", result);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+#else
+int main()
+{
+    int num_stalls;                   // N [2; 100 000]
+    int num_cows;                     // C [2; N]
     uint32_t a_stall[MAX_NUM_STALLS]; // Stall positions [x1; xN], where xi [0; 1 000 000 000]
 
     int num_testcases;
@@ -105,3 +132,4 @@ int main()
 
     return 0;
 }
+#endif
